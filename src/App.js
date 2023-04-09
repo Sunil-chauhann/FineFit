765d8fd0d626f4dbc85fded5e9992214dc804992
Sunil Chauhan <101030831+Sunil-chauhann@users.noreply.github.com>
2023-04-09 14:00:52 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-  // Routes
-} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./style.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "context/AuthContext";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children
+  };
+
   return (
-    <div>
-    <Router>
-    <Navbar/>
-    <Switch>
-          <Route exact path="/home"><News key = "General" pagesize={15} country = "in" category="General"/></Route>
-          <Route exact path="/"><News key = "General1" pagesize={15} country = "in" category="General"/></Route>
-          <Route exact path="/sports"><News key = "Sports" pagesize={15} country = "in" category="Sports"/></Route>
-          <Route exact path="/entertainment"><News key = "Entertainment" pagesize={15} country = "in" category="Entertainment"/></Route>
-          <Route exact path="/science"><News key = "Science"  pagesize={15} country = "in" category="Science"/></Route>
-          <Route exact path="/business"><News key = "Business" pagesize={15} country = "in" category="Business"/></Route>
-          
-        </Switch>
-    </Router>
-  </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-<link to=""> </link>
